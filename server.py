1414b728ca82870a6306dc4265ac198999ff6451
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-03-13 20:42:23 +0300

    Added function for the server to accept client requests

--- server.py
+++ server.py
@@ -1,19 +1,29 @@
 import socket
+from threading import Thread
 
 host = "localhost"
 port = 8080
+clients = {}
+addresses = {}
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 
-sock.listen(1)
 
-print("The server is up and running, so it is listening to clients requests")
+def accept_client_connections():
+    while True:
+        client_conn, client_address = sock.accept()
+        print(client_address, "Has connected")
+        client_conn.send(
+            "Welcome to the Chat Romm, Please type your name to continue".encode('utf8'))
+        addresses[client_conn] = client_address
 
-conn, address = sock.accept()
 
-message = "Hey there, I have a message for you"
+if __name__ == "__main__":
+    sock.listen(5)
+    print("The server is running and is listening to clients requests")
 
-conn.send(message.encode())
-
-conn.close()
+    t1 = Thread(target=accept_client_connections)
+    t1.start()
+    t1.join()
