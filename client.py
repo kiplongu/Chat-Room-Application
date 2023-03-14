25dfefd87c594689ebfd0f869c9695435ef6ad93
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-03-14 10:09:40 +0300

    Added TK functionalitis on the app window

--- client.py
+++ client.py
@@ -1,15 +1,49 @@
+import tkinter
 import socket
+from tkinter import *
+from threading import Thread
 
-host = "localhost"
-port = 8080
+window = Tk()
+window.title("Chat Room Application")
+window.configure(bg="green")
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(host, port)
+message_frame = Frame(window, height=100, width=100, bg="red")
+message_frame.pack()
 
-message = sock.recv(1024)
+my_msg = StringVar()
+my_msg.set("")
 
-while message:
-    print('Mesaage:', message.decode())
-    message = sock.recv(1024)
+scroll_bar = Scrollbar(message_frame)
+msg_list = Listbox(message_frame, height=15, width=100,
+                   bg="red", yscrollcommand=scroll_bar.set)
+scroll_bar.pack(side=RIGHT, fill=Y)
+msg_list.pack(side=LEFT, fill=BOTH)
+msg_list.pack()
 
-sock.close()
+label = Label(window, text="Enter the Message",
+              fg="blue", font="Aeria", bg="yellow")
+label.pack()
+
+entry_field = Entry(window, textvariable=my_msg, fg="red", width=50)
+entry_field.pack()
+
+send_Button = Button(window, text="Send", font="Aerial",
+                     fg="white", command=send)
+send_Button.pack()
+
+quit_Button = Button(window, text="Quit", font="Aerial",
+                     fg="white", command=on_closing)
+quit_Button.pack()
+
+Host = "127.0.0.1"
+Port = 8080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((Host, Port))
+
+
+receive_Thread = Thread(target=receive)
+receive_Thread.start()
+
+
+mainloop()
